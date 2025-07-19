61c2d203fc09740f9706126a426c93266e99a279
Tejasshelke1770 <shelket17@gmail.com>
2025-07-19 18:05:43 +0530

    final commit and pending saved functionality and profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { icons } from "@/constants/icons";
 
 const profile = () => {
   return (
-    <View className="bg-primary flex-1 flex-row items-center justify-center">
-      <Text className="text-white font-bold">under maintainance</Text>
+    <View className="bg-primary flex-1 ">
+      <View className="flex flex-1 items-center justify-center gap-5 flex-col ">
+        <Image source={icons.person} className="size-10" tintColor="#FFF" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
     </View>
   );
 };
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { icons } from "@/constants/icons";
 
 const saved = () => {
   return (
-    <View className="bg-primary flex-1 flex-row items-center justify-center">
-      <Text className="text-white font-bold">under maintainance</Text>
+    <View className="bg-primary flex-1">
+      <View className="flex-1 flex items-center justify-center gap-5 flex-col ">
+        <Image source={icons.save} className="size-10" tintColor="#FFF" />
+        <Text className="text-gray-500 text-base">
+          Saved Movies will Appear here
+        </Text>
+      </View>
     </View>
   );
 };
